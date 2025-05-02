4c9fdda0a704caac5c53515f7fddffa590d1c039
Oleg Pona <olegpona606@gmail.com>
2025-05-02 16:52:16 +0300

    add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,65 @@ class Program
     {
         using (var context = new Context())
         {
-            
+            Console.WriteLine("Welcome to the Library Manager!");
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("Books:");
+                Console.WriteLine("1. Books list");
+                Console.WriteLine("2. Add book");
+                Console.WriteLine("3. Update info about book");
+                Console.WriteLine("4. Delete book");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Users:");
+                Console.WriteLine("5. Users list");
+                Console.WriteLine("6. Register new user");
+                Console.WriteLine("7. Update info about user");
+                Console.WriteLine("8. Delete user");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Books Operations:");
+                Console.WriteLine("9.  Borrow a book to user");
+                Console.WriteLine("10. Return a book to library");
+                Console.WriteLine("11. View all borrows books");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("0. Exit");
+                int a = int.Parse(Console.ReadLine());
+
+                switch (a)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
         }
     }
 }
+
+// git add .
+// git commit -m "Initial commit"
+// git push origin main
